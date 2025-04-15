3b273b994adfdb73544069f3130e8f9295f7259a
nagytommy76 <nagytommy76@gmail.com>
2025-04-15 19:38:37 +0200

    Set light positions

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -6,15 +6,14 @@ import LotusCar from '@/models/LotusCar'
 export default function HomePage() {
    return (
       <section className='h-screen'>
-         <h1 className='text-4xl font-bold'>Three.js Portfolio</h1>
-         <Canvas className='bg-transparent' camera={{ near: 0.1, far: 1000 }}>
+         <Canvas className='bg-transparent w-full h-full' camera={{ near: 0.1, far: 1000 }}>
             <Suspense fallback={null}>
-               <directionalLight />
-               <ambientLight />
-               <pointLight />
-               <spotLight />
-               <hemisphereLight />
-               <LotusCar />
+               <directionalLight position={[1, 4, 1]} intensity={2} />
+               <ambientLight intensity={0.5} />
+               {/* <pointLight position={[10, 5, 10]} intensity={2} /> */}
+               {/* <spotLight position={[0, 50, 10]} angle={0.77} penumbra={1} intensity={2} /> */}
+               <hemisphereLight color={'#ffffff'} groundColor='#000000' intensity={1} />
+               <LotusCar position={[-1, 0, 0]} scale={[120, 120, 120]} rotation={[0.3, 1, 0]} />
             </Suspense>
          </Canvas>
       </section>

dfa4524dae30d8f751d7d83eb81e8580f8d0f079
nagytommy76 <nagytommy76@gmail.com>
2025-04-16 18:38:29 +0200

    Set ambientLight intensity

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -6,14 +6,14 @@ import LotusCar from '@/models/LotusCar'
 export default function HomePage() {
    return (
       <section className='h-screen'>
-         <Canvas className='bg-transparent w-full h-full' camera={{ near: 0.1, far: 1000 }}>
+         <Canvas className='bg-transparent w-full h-full' camera={{ near: 0.1, far: 100 }}>
             <Suspense fallback={null}>
                <directionalLight position={[1, 4, 1]} intensity={2} />
-               <ambientLight intensity={0.5} />
-               {/* <pointLight position={[10, 5, 10]} intensity={2} /> */}
-               {/* <spotLight position={[0, 50, 10]} angle={0.77} penumbra={1} intensity={2} /> */}
-               <hemisphereLight color={'#ffffff'} groundColor='#000000' intensity={1} />
-               <LotusCar position={[-1, 0, 0]} scale={[120, 120, 120]} rotation={[0.3, 1, 0]} />
+               <ambientLight intensity={0.1} />
+               {/* <pointLight position={[10, 5, 10]} intensity={2} />
+               <spotLight position={[0, 50, 10]} angle={0.77} penumbra={1} intensity={2} /> */}
+               <hemisphereLight color={'#ffffff'} groundColor='#000000' intensity={0.4} />
+               <LotusCar position={[0, 0, 0]} scale={[1, 1, 1]} rotation={[0, 0, 0]} />
             </Suspense>
          </Canvas>
       </section>

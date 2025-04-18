638ea68893324f07529e45f6f34fb148751b3198
nagytommy76 <nagytommy76@gmail.com>
2025-04-18 13:00:08 +0200

    Using ScrollControls

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { Canvas } from '@react-three/fiber'
+import { ScrollControls } from '@react-three/drei'
 import { Suspense } from 'react'
 import LotusCar from '@/models/LotusCar'
 
@@ -7,13 +8,13 @@ export default function HomePage() {
    return (
       <section className='h-screen'>
          <Canvas className='bg-transparent w-full h-full' camera={{ near: 0.1, far: 100 }}>
+            <directionalLight position={[1, 4, 1]} intensity={2} />
+            <ambientLight intensity={0.5} />
+            <hemisphereLight color={'#ffffff'} groundColor='#000000' intensity={0.4} />
             <Suspense fallback={null}>
-               <directionalLight position={[1, 4, 1]} intensity={2} />
-               <ambientLight intensity={0.1} />
-               {/* <pointLight position={[10, 5, 10]} intensity={2} />
-               <spotLight position={[0, 50, 10]} angle={0.77} penumbra={1} intensity={2} /> */}
-               <hemisphereLight color={'#ffffff'} groundColor='#000000' intensity={0.4} />
-               <LotusCar position={[0, 0, 0]} scale={[1, 1, 1]} rotation={[0, 0, 0]} />
+               <ScrollControls pages={3} damping={0.25}>
+                  <LotusCar position={[0, 0, 0]} scale={[1, 1, 1]} rotation={[0, 0, 0]} />
+               </ScrollControls>
             </Suspense>
          </Canvas>
       </section>

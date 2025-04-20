ab6e59ba21d1f5cc2e4d1eb9a25c5c5d015420b2
nagytommy76 <nagytommy76@gmail.com>
2025-04-20 14:27:45 +0200

    Using Mars model

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -2,18 +2,20 @@
 import { Canvas } from '@react-three/fiber'
 import { ScrollControls } from '@react-three/drei'
 import { Suspense } from 'react'
-import LotusCar from '@/models/LotusCar'
+// import LotusCar from '@/models/LotusCar'
+import Mars from '@/models/Mars'
 
 export default function HomePage() {
    return (
       <section className='h-screen'>
          <Canvas className='bg-transparent w-full h-full' camera={{ near: 0.1, far: 100 }}>
-            <directionalLight position={[1, 4, 1]} intensity={2} />
-            <ambientLight intensity={0.5} />
-            <hemisphereLight color={'#ffffff'} groundColor='#000000' intensity={0.4} />
+            <directionalLight color={'#ffffff'} position={[-0.5, -0.5, 1]} intensity={4} />
+            {/* <ambientLight intensity={1} /> */}
+            <hemisphereLight color={'#ffffff'} groundColor='#000000' intensity={0.2} />
             <Suspense fallback={null}>
-               <ScrollControls pages={3} damping={0.25}>
-                  <LotusCar position={[0, 0, 0]} scale={[1, 1, 1]} rotation={[0, 0, 0]} />
+               <ScrollControls pages={2} damping={0.25}>
+                  {/* <LotusCar position={[0, 0, 0]} scale={[1, 1, 1]} rotation={[0, 0, 0]} /> */}
+                  <Mars position={[0, 0, 0]} scale={[1, 1, 1]} rotation={[0, 0, 0]} />
                </ScrollControls>
             </Suspense>
          </Canvas>

da16dbf23411c67ddc5242f594f315af5c44f1cc
nagytommy76 <nagytommy76@gmail.com>
2025-05-14 20:09:01 +0200

    Import Blender room model

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -2,17 +2,17 @@
 import { Canvas } from '@react-three/fiber'
 import { ScrollControls } from '@react-three/drei'
 import { Suspense } from 'react'
-import Mars from '@/models/Mars'
+import Room from '@/models/Room'
 
 export default function HomePage() {
    return (
       <section className='h-screen'>
          <Canvas className='bg-transparent w-full h-full' camera={{ near: 0.1, far: 100 }}>
-            <directionalLight color={'#ffffff'} position={[-0.13, -0.1, 0]} intensity={4} />
-            <hemisphereLight color={'#ffffff'} groundColor='#000000' intensity={0.2} />
+            <directionalLight color={'#ffffff'} position={[-1, 0, 0]} intensity={4} />
+            <hemisphereLight color={'#ffffff'} groundColor='#000000' intensity={1} />
             <Suspense fallback={null}>
                <ScrollControls pages={2} damping={0.25}>
-                  <Mars position={[0, 0, 0]} scale={[0.7, 0.7, 0.7]} rotation={[0, 0, 0]} />
+                  <Room position={[0, 0, 0]} scale={[1, 1, 1]} rotation={[0.5, -0.5, 0]} />
                </ScrollControls>
             </Suspense>
          </Canvas>

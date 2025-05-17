30ab7166f124a4830ae15277ed8afe91920c2516
nagytommy76 <nagytommy76@gmail.com>
2025-05-17 12:10:49 +0200

    Using orbit controls

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { Canvas } from '@react-three/fiber'
-import { ScrollControls } from '@react-three/drei'
+import { OrbitControls, Environment } from '@react-three/drei'
 import { Suspense } from 'react'
 import Room from '@/models/Room'
 
@@ -8,13 +8,23 @@ export default function HomePage() {
    return (
       <section className='h-screen'>
          <Canvas className='bg-transparent w-full h-full' camera={{ near: 0.1, far: 100 }}>
-            <directionalLight color={'#ffffff'} position={[-1, 0, 0]} intensity={4} />
-            <hemisphereLight color={'#ffffff'} groundColor='#000000' intensity={1} />
+            {/* <directionalLight color={'#ffffff'} position={[-1, 0, 0]} intensity={4} />
+            <hemisphereLight color={'#ffffff'} groundColor='#000000' intensity={1} /> */}
             <Suspense fallback={null}>
-               <ScrollControls pages={2} damping={0.25}>
-                  <Room position={[0, 0, 0]} scale={[1, 1, 1]} rotation={[0.5, -0.5, 0]} />
-               </ScrollControls>
+               {/* <ScrollControls pages={2} damping={0.25}> */}
+               <OrbitControls />
+               <Room
+                  position={[0, 0, 0]}
+                  scale={[1, 1, 1]}
+                  // rotateX={0.9}
+                  // rotateY={0.9}
+                  // rotateZ={0.7}
+                  // rotation={[0.5, -0.5, 0]}
+                  rotation={[0, 0, 0]}
+               />
+               {/* </ScrollControls> */}
             </Suspense>
+            <Environment preset='city' />
          </Canvas>
       </section>
    )

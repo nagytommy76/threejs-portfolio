0c10f6ba42b5bcbb582be3bdff0ef1f4581fde50
nagytommy76 <nagytommy76@gmail.com>
2025-05-19 19:45:42 +0200

    Set up lights
    Playing with the optimal light setup

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -1,6 +1,6 @@
 'use client'
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls, Environment, ContactShadows } from '@react-three/drei'
+import { OrbitControls, SoftShadows, ContactShadows } from '@react-three/drei'
 import { Suspense } from 'react'
 import Room from '@/models/Room'
 
@@ -8,22 +8,28 @@ export default function HomePage() {
    return (
       <section className='h-screen'>
          <Canvas className='bg-transparent w-full h-full' camera={{ near: 0.1, far: 100 }}>
-            {/* <directionalLight color={'#ffffff'} position={[-1, 0, 0]} intensity={4} />
-            <hemisphereLight color={'#ffffff'} groundColor='#000000' intensity={1} /> */}
+            <directionalLight
+               color={'#ffffff'}
+               position={[1, 1, 1]}
+               intensity={3}
+               castShadow
+               shadow-mapSize={2048}
+               shadow-bias={-0.001}
+            />
+            {/* <spotLight color={'#ffffff'} position={[5, -2, 0]} intensity={200} angle={0.5} penumbra={1} /> */}
+            {/* <hemisphereLight color={'#ffffff'} groundColor='#000000' intensity={2.5} />
+            <ambientLight intensity={0.4} /> */}
+            <SoftShadows samples={10} />
             <Suspense fallback={null}>
-               {/* <ScrollControls pages={2} damping={0.25}> */}
-               <OrbitControls />
-               <Room
-                  position={[0, 0, 0]}
-                  scale={[1, 1, 1]}
-                  // rotateX={0.9}
-                  // rotateY={0.9}
-                  // rotateZ={0.7}
-                  // rotation={[0.5, -0.5, 0]}
-                  rotation={[0, 0, 0]}
+               <OrbitControls
+                  enablePan={true}
+                  enableZoom={true}
+                  minPolarAngle={Math.PI / 52}
+                  maxPolarAngle={Math.PI / 2.4}
+                  maxDistance={5}
                />
-               {/* </ScrollControls> */}
-               <Environment preset='city' />
+               <Room position={[0, 0, 0]} scale={[1, 1, 1]} rotation={[0, 0, 0]} />
+               {/* <Environment preset='city' /> */}
             </Suspense>
             <ContactShadows position={[0, -4.5, 0]} scale={20} blur={2} far={4.5} />
          </Canvas>

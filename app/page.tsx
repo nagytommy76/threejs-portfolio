18d0fbda4f25f8d3f32187d610301e75c6d5b0a0
nagytommy76 <nagytommy76@gmail.com>
2025-06-04 19:44:40 +0200

    temporarily add 3 page sections

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
 import HomePage from '@/components/HomePage/HomePage'
 
 export default function Home() {
-   return <HomePage />
+   return (
+      <section>
+         <HomePage />
+         <section className='second-scroll h-screen relative'>
+            <h1>Második oldal CSÁ</h1>
+         </section>
+         <section className='third-scroll h-screen relative'>
+            <h1>Harmadik oldal</h1>
+         </section>
+      </section>
+   )
 }

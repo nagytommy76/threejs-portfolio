b8d5f28298a4e11c229a22598a505b1bbf480c97
nagytommy76 <nagytommy76@gmail.com>
2025-06-07 13:02:11 +0200

    Add TechStackPage

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,14 @@
 import HomePage from '@/components/HomePage/HomePage'
+import TechStackPage from '@/components/TechStackPage'
 
 export default function Home() {
    return (
-      <section>
+      <>
          <HomePage />
-         <section className='second-scroll h-screen relative'>
-            <h1>Második oldal CSÁ</h1>
-         </section>
+         <TechStackPage />
          <section className='third-scroll h-screen relative'>
             <h1>Harmadik oldal</h1>
          </section>
-      </section>
+      </>
    )
 }

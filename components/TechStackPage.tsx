18d6ed9fc185d466c57ad0c1a7970e015ba8eb4b
nagytommy76 <nagytommy76@gmail.com>
2025-06-07 13:01:24 +0200

    Created tech stack component

---
+++ components/TechStackPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function TechStackPage() {
+   return (
+      <section className='second-scroll h-screen relative'>
+         <h1>Második oldal CSÁ</h1>
+      </section>
+   )
+}

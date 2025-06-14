5f485801b293ba14806133ea5829ed9467b4741a
nagytommy76 <nagytommy76@gmail.com>
2025-06-14 13:41:52 +0200

    using ScrollToPlugin

--- components/HomePage/HomePage.tsx
+++ components/HomePage/HomePage.tsx
@@ -6,7 +6,9 @@ import Room from '@/models/Room'
 
 import { gsap } from 'gsap'
 import ScrollTrigger from 'gsap/ScrollTrigger'
+import ScrollToPlugin from 'gsap/ScrollToPlugin'
 gsap.registerPlugin(ScrollTrigger)
+gsap.registerPlugin(ScrollToPlugin)
 
 export default function HomePage() {
    return (
